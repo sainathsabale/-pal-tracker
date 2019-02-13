50c69af8621c1f532315467faa0dc1effb6987ae
Sainath Baban Sabale - (Digital) <SA20067648@wipro.com>
2019-02-13 16:36:55 +0530

    CRUD New Files

---
+++ src/main/java/io/pivotal/pal/tracker/InMemoryTimeEntryRepository.java
@@ -0,0 +1,25 @@
+package io.pivotal.pal.tracker;
+
+public class InMemoryTimeEntryRepository implements TimeEntryRepository {
+
+
+    @Override
+    public void insertTimeEntry() {
+
+    }
+
+    @Override
+    public void getTimeEntry() {
+
+    }
+
+    @Override
+    public void updateTimeEntry() {
+
+    }
+
+    @Override
+    public void deleteTimeEntry() {
+
+    }
+}
